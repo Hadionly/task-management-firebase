e8785f3c028ad3217c0b4c6dc52ebcc135d7d7da
Hadi Elamin <hb5953@wayne.edu>
2025-01-11 22:58:07 +0200

    Refactor App component to remove Redux Provider and simplify navigation setup

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TaskForm from "./pages/TaskForm";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 import Home from "./pages/Home";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { RootStackParamList } from "./interfaces/navigation.types";
@@ -15,23 +13,21 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <GestureHandlerRootView style={{ flex: 1 }}>
-          <Provider store={store}>
-            <NavigationContainer>
-              <Stack.Navigator
-                screenOptions={{
-                  headerShown: false,
-                }}>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen
-                  name="TaskForm"
-                  component={TaskForm}
-                  options={{
-                    title: "Task Form",
-                  }}
-                />
-              </Stack.Navigator>
-            </NavigationContainer>
-          </Provider>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+              }}>
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen
+                name="TaskForm"
+                component={TaskForm}
+                options={{
+                  title: "Task Form",
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
         </GestureHandlerRootView>
       </SafeAreaView>
     </SafeAreaProvider>
